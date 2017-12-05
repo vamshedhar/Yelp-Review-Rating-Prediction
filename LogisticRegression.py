567850bde76dd09514738b7d2601da47feb7047b
Ushaswini <ushaswini.vinnakota@gmail.com>
2017-12-04 22:23:21 -0500

    Added comments

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -18,38 +18,43 @@ from operator import itemgetter
 
 from sklearn.linear_model import LogisticRegression as SKLogisticRegression
 
+
 def sigmoid(z):
 		return 1 / (1 + np.exp(-z)) 
-
-def check_ylabel(x, i):
+'''Function to check y-label and return 1 for class i and 0 for other classes'''
+def check_rating(x, i):
 	if(x[0]==i):
 		x[0]=1
 	else:
 		x[0]=0
 	return x
 
-def gradiant_fun(inputVector, beta):
-	y_vec = inputVector[0]
-	inputVector[0] = 1.0
-	X = np.asmatrix(inputVector, dtype=float).T
+'''Function to calculate gradient'''
+def calculate_gradient(input_vector, beta):
+	rating_current_review = input_vector[0]
+	input_vector[0] = 1.0
+	X = np.asmatrix(input_vector, dtype=float).T
 	scores = np.dot(beta.T,X)
-	residue = y_vec - sigmoid(scores)
-	gradiant = np.dot(X,residue)
-	return gradiant
+	residue = rating_current_review - sigmoid(scores)
+	gradient = np.dot(X,residue)
+	return gradient
 
-def predict(x_test, beta_all):
-	x = np.asarray(x_test)
+'''Function to pedict class for test input using beta vectors calculated with training data'''
+def predict_rating(test_review, beta_all):
+	x = np.asarray(test_review)
 	x = np.append([1], x)
 	x = x.reshape(1, x.shape[0])
-	values = [(np.dot(x, w.T), class_label) for w, class_label in beta_all]
-	return max(values, key=itemgetter(0))[1]
+	h_beta_x = [(np.dot(x, beta.T), class_label) for beta, class_label in beta_all]
+	return max(h_beta_x, key=itemgetter(0))[1]
 
-def reduceMatrix(x):
-	a = np.asarray(x)
-	return a[0]
+'''Function to convert matrix to array'''
+def reduceMatrix(input_matrix):
+	converted_array = np.converted_arraysarray(input_matrix)
+	return converted_array[0]
 
 class LogisticRegression:
 
+	'''Dunction to initialize all class level variables'''
 	def __init__(self, filepath, classes):
 		self.filepath = filepath
 		self.classes = classes
@@ -58,6 +63,7 @@ class LogisticRegression:
 
 		self.sc = SparkContext.getOrCreate()
 
+	'''Function to convert 5 class labels to 3 class labels if 3 class classification is chosen'''
 	def convert_rating(self, rating):
 		intRating = int(rating[0])
 
@@ -71,7 +77,7 @@ class LogisticRegression:
 			else:
 				return 2
 
-
+	''' Function to load data from csv file and prepare input data structures'''
 	def load_data(self):
 		print ("Loading data fron File:")
 		total_lines = sum([1 for row in open(self.filepath, 'rU')]) - 1
@@ -88,10 +94,12 @@ class LogisticRegression:
 					print( str(math.ceil(percent * 100)) + "% complete")
 					percent += 0.1
 
+	'''Function to run Logistic Regression model'''
 	def run_model(self):
 
 		self.load_data()
 
+		'''Calculate TF-IDF values removing English stop words and considering bi-gram'''
 		tfidf_vectorizer = TfidfVectorizer(max_df=0.90, max_features=500, stop_words='english', use_idf=True, tokenizer=nltk.word_tokenize, ngram_range=(2,2))
 
 		tfidf_data = tfidf_vectorizer.fit_transform(self.reviews)
@@ -103,46 +111,50 @@ class LogisticRegression:
 
 		y_matrix = y_train.reshape(X_train.shape[0], 1)
 
-		X_y = sp.sparse.hstack((y_matrix, X_train))
+		training_data = sp.sparse.hstack((y_matrix, X_train))
 
-		X_y.tocsr()
+		training_data.tocsr()
 
-		Xy = self.sc.parallelize(X_y.tocsr().todense())
+		data_parallelized = self.sc.parallelize(training_data.tocsr().todense())
 
 		beta_all = []
 
+		'''Calculating Beta values for each class'''
 		for i in np.unique(y):
 			beta = np.zeros(X_train.shape[1] + 1).reshape(X_train.shape[1] + 1, 1)  
-			Xy2 = Xy.map(lambda x: reduceMatrix(x))
-			Xy_new = Xy2.map(lambda x: check_ylabel(x,i))
+			data_transformed = data_parallelized.map(lambda x: reduceMatrix(x))
+			Xy_two_class = data_transformed.map(lambda x: check_rating(x,i))
 
-			num_iter = 10
+			number_iterations = 10
 			alpha = 0.001
 			
 			print("class " + str(i))
 
-			for j in range(num_iter):
+			'''Calculate beta values for a class'''
+			for j in range(number_iterations):
 				print("iteration " + str(j))
-				gradiant=Xy_new.map(lambda x: gradiant_fun(x, beta))
-				gradiant = gradiant.reduce(lambda a,b:a+b)
-				beta=beta+alpha*gradiant
+				gradient=Xy_two_class.map(lambda x: calculate_gradient(x, beta))
+				gradient = gradient.reduce(lambda a,b:a+b)
+				beta=beta+alpha*gradient
 
 			beta_all.append((beta.T,i))
 
-		X_test2 = self.sc.parallelize(X_test.todense())
+		test_data = self.sc.parallelize(X_test.todense())
 
 		self.beta_all = beta_all
 
-		y_pred = X_test2.map(lambda x: predict(x, beta_all)).collect()
-		y_pred = np.asarray(y_pred)
+		y_predicted = test_data.map(lambda x: predict_rating(x, beta_all)).collect()
+		y_predicted = np.asarray(y_predicted)
 
-		#calculating accuracy   
-		print('Accuracy from scratch: {0}'.format((y_pred == y_test.T).sum().astype(float) / len(y_test)))
+		'''Calculating accuracy using model built'''
+		print('Accuracy from scratch: {0}'.format((y_predicted == y_test.T).sum().astype(float) / len(y_test)))
 
+		'''Calculating accuracy using scikit learn libraries'''
 		clf = SKLogisticRegression(fit_intercept=True, C = 1e15)
 		clf.fit(X_train, y_train)
 		print('Accuracy from sk-learn: {0}'.format(clf.score(X_test, y_test)))
 
+	'''Predicting rating for a test review '''
 	def predict_rating(self, review):
 		new_reviews = self.reviews + [review]
 		tfidf_vectorizer = TfidfVectorizer(max_df=0.90, max_features=500, stop_words='english', use_idf=True, tokenizer=nltk.word_tokenize, ngram_range=(2,2))
@@ -154,6 +166,7 @@ class LogisticRegression:
 
 if __name__ == "__main__":
 
+	'''Create Logistic Regression model and run'''
 	model = LogisticRegression("reviews.csv", 5)
 
 	model.run_model()
